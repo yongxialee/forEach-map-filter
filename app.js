399eefeb4bac54e8505db745e3e82fa48ead08ff
yongxialee leelianou <dejnaj@hotmail.com>
2022-08-12 10:30:36 -0600

    add question description to app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // double values
+//Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
 function doubleValues(array){
     let newArray=[];
     array.forEach(function(val){
@@ -8,6 +9,7 @@ function doubleValues(array){
 }
 
 //only eben values
+//Write a function called onlyEvenValues which accepts an array and returns a new array with only the even values in the array passed to the function
 function onylyEvenValues(arr){
     let newArr=[];
         arr.forEach(function(val){
@@ -19,6 +21,7 @@ function onylyEvenValues(arr){
         }
          
 // show first and last
+//Write a function called showFirstAndLast which accepts an array of strings and returns a new array with only the first and last character of each string.
 
 function showFirstAndLast(arr){
     const showArr=[];
@@ -29,12 +32,8 @@ function showFirstAndLast(arr){
 }
 
 // add key and value
-const arr=[
-    {name: 'Elie'},
-    {name: 'Tim'},
-    {name: 'Matt'},
-    {name: 'Happy'}
-];
+//Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed to the function with the new key and value added for each object
+
 function addKeyAndValue(arr,key,value){
     arr.forEach(function(val){
         val[key]=value;
@@ -43,6 +42,7 @@ function addKeyAndValue(arr,key,value){
 }
 
 //vowelcount
+//Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
 function vowelCount(str){
     let splitArr=str.split("");
     
@@ -64,6 +64,7 @@ function vowelCount(str){
 }
 // map
 //double value with map
+//Write a function called doubleValuesWithMap which accepts an array and returns a new array with all the values in the array passed to the function doubled
 function doubleValuesWithMap(arr){
     let doubleArr=[];
     arr.map(function(val){
@@ -73,6 +74,7 @@ return doubleArr;
 }
 
 //val times index
+//Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.
 function valTimesIndex(arr){
 return arr.map(function(val,i){
     return val * i;
@@ -81,6 +83,7 @@ return arr.map(function(val,i){
 }
 
 //extractkey
+//Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value of that key in each object.
 
 function extractKey(arr,key){
  return arr.map(function(val){
@@ -88,6 +91,7 @@ function extractKey(arr,key){
  });
 }
 // extract full name
+//Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the key with a name of “first” and the value of a key with the name of “last” in each object, concatenated together with a space.
 function extractFullName(arr){
     return arr.map(function(full){
         return full.first+ " "+ full.last;
@@ -96,6 +100,7 @@ function extractFullName(arr){
 
 //filter 
 //filter by value
+//Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
 function filterByValue(arr,key){
  //   let filterArr=[];
     return arr.filter(function(val){
@@ -107,6 +112,9 @@ function filterByValue(arr,key){
 
     }
 //find 
+//Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
+
+
 function find(arr,searchValue){
  return arr.filter(function(val){
 
@@ -119,6 +127,7 @@ function find(arr,searchValue){
 }
 
 //findInObj   
+//Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
 
    function findInObj(arr, key, searchValue) {
     return arr.filter(function(val) {
@@ -126,6 +135,7 @@ function find(arr,searchValue){
     })[0];
   }
   // remove vowels 
+  //Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
   function removeVowels(str){
     const vowels='aeiou';
     let lowerCaseletter=str.toLowerCase();
@@ -137,6 +147,7 @@ function find(arr,searchValue){
   }
 
   // double Odd Number
+  //Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and fitler to double and then filter the odd numbers).
   function doubleOddNumbers(arr){
     return arr.filter(function(val){
        return val %2 !==0;
